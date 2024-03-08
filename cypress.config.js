4ad59a51be92d4e9a0f7a2e28483f57e2741168b
MartinSWDev <martinsoftwaredev@outlook.com>
2024-03-08 11:40:54 +0000

    test: add navbar sticky tests (failing)

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     fixturesFolder: "cypress/fixtures",
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
