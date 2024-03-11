7b70b1df11a0260833ecd407e6cb5aeb78b31f21
MartinSWDev <martinsoftwaredev@outlook.com>
2024-03-11 10:27:31 +0000

    fix: vite config for dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ export default {
   server: {
     port: 5173,
   },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name]-[hash].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 };
 
