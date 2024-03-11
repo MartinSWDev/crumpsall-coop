b8f5c030a9fdc556631ff4bb87dd561e5e9fddf5
Martin William <martinsoftwaredev@outlook.com>
2024-03-11 10:21:59 +0000

    Update vite.config.js
    remove hashed html, js, css

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ export default {
   server: {
     port: 5173,
   },
+  build: {
+    emptyOutDir: true,
+    outDir: "./dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name]-[hash].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 };
 
